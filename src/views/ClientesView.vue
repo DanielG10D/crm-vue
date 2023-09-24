1566d305a7bc1db1ccbf22bdd2e7dfcecbeddc73
Danie <danielguipe757@gmail.com>
2023-09-24 19:21:28 -0400

    Se agrega el Proyecto1

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { } from 'vue'
+  
+    import RouterLink from '../components/UI/RouterLink.vue'
+    import Heading from '../components/UI/Heading.vue'
+
+
+
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
+
+</script>
+
+<template>
+
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="agregar-cliente">
+                Agregar Cliente
+            </RouterLink>
+           
+        </div>
+       
+        
+
+        <Heading>{{ titulo }}</Heading>
+    </div>
+</template>
